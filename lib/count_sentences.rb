1926e7cd30d7b23ec74b4b7e6395e7c793c43fa4
somaia-khalil <sumaya_khair@live.com>
2020-01-31 20:56:09 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,39 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[length-1] == "."
+      return true
+    else
+      false
+    end
   end
 
   def question?
+    if self[length-1] == "?"
+      return true
+    else
+      false
+    end
 
   end
 
   def exclamation?
+    if self[length-1] == "!"
+      return true
+    else
+      false
+    end
 
   end
 
   def count_sentences
-
-  end
+    char = self.split.select do |s|
+      s.sentence? || s.question? || s.exclamation? 
+    end
+    char.count
+  end 
+  
+  # binding.pry
 end
+
+
